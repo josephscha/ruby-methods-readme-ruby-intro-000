96e416b2fb5a998c242b113fdf6cb1fdfa158bc7
Joseph S. Cha <josephshincha@gmail.com>
2019-12-15 00:35:11 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,13 +1,12 @@
 # Use this space to code along with the readme
 
-phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+def hello_world_five_times
+  phrase = "Hello World"
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+end
+
+hello_world_five_times
